96fbcf958b5f06b22782349f8be653e5f3789458
annyup <x-annypham@live.com>
2020-06-22 20:15:09 -0400

    Updates

---
+++ precache-manifest.cd41b1c3352cb4e6b18493445865d448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161dbeb72f9705f58ae3b1161f20a930",
+    "url": "/pic-draw-sso/index.html"
+  },
+  {
+    "revision": "b209aacdb3fbf9e76343",
+    "url": "/pic-draw-sso/static/css/main.f784fefb.chunk.css"
+  },
+  {
+    "revision": "eb1c6ca4766c70a3024e",
+    "url": "/pic-draw-sso/static/js/2.6a7e7168.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/pic-draw-sso/static/js/2.6a7e7168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b209aacdb3fbf9e76343",
+    "url": "/pic-draw-sso/static/js/main.e254eb2f.chunk.js"
+  },
+  {
+    "revision": "08a97e3a28d98fb6f0d8",
+    "url": "/pic-draw-sso/static/js/runtime-main.b174e85e.js"
+  },
+  {
+    "revision": "8e7bbd260f312e956b0ef6d1318e3434",
+    "url": "/pic-draw-sso/static/media/triangle-bg.8e7bbd26.png"
+  }
+]);
